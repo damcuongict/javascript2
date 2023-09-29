6200fa8645615f682284d7ce1ee978491c13d8f8
cuongdvph30239 <damcuongict@gmail.com>
2023-09-29 10:46:56 +0700

    Thêm các tệp .vscode/settings.json, bai21.html, bai21.js, bai22.html, bai22.js, bai23.html, bai23.js, bai24.html

---
+++ bai21.js
@@ -0,0 +1,28 @@
+const gameEvents = new Map([
+  [17,'GOAL'],
+  [36,'substitution'],
+  [47,'GOAL'],
+  [61,'substitution'],
+  [64,'Yellow card'],
+  [69,'Red card'],
+  [70,'substitution'],
+  [72,'substitution'],
+  [76,'GOAL'],
+  [80,'GOAL'],
+  [92,'Yellow card'],
+
+]);
+//1
+const events = new Set(gameEvents.values());
+console.log(events);
+//2
+gameEvents.delete(64);
+//3
+console.log(
+  `An event happened, on average, every ${90/gameEvents.size} minutes`
+);
+// 4
+for(const[min,event] of gameEvents){
+  const half = min<=45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min} : ${event}`);
+}
